50da9f86972d193a160723ff3e7a7de0b3e88b34
dragonjamez <jamey_brandon2@icloud.com>
2026-04-29 14:02:10 -0400

    uhhh day 2 i think

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 startbtn = document.createElement("button")
+startbtn.id = "shop"
 startbtn.innerText = "start playing"
+shopclosebtn = document.createElement("button")
+shopclosebtn.innerText = "close shop"
+shopclosebtn.style.visibility = 'hidden'
 startbtn.addEventListener("click",() => {
     startbtn.remove()
     if (localStorage.thetimer == "NaN") 
@@ -10,14 +14,14 @@ startbtn.addEventListener("click",() => {
   }
     timerText.textContent = "you have wasted time for " + localStorage.thetimer + " seconds"
     interval = setInterval(timer,1000)
+    document.body.appendChild(shopbtn)
+    document.body.appendChild(shopclosebtn)
     
 })
 document.body.appendChild(startbtn)
 timerText = document.createElement("timerText")
 document.body.appendChild(timerText)
 interval = 0 
-shopbtn = document.createElement("button")
-shopbtn.innerText = "open shop"
 function timer() {
     if (localStorage.thetimer == "NaN") {
     localStorage.thetimer = 0;
@@ -30,15 +34,37 @@ function timer() {
 }
 shopbtn = document.createElement("button")
 shopbtn.innerText = "open shop"
+function changebg(){
+    color = Math.floor(Math.random()* 16777215).toString(16)
+    color = "#"+color
+    document.body.style.backgroundColor = color
+}
 class item{
-    constructor(name,cost,onetime){
+    constructor(name,cost,func){
         this.name = name
         this.cost = cost
-        this.onetime = onetime
-        this.button.innerText = name
-    }
-    buy(){
-       localStorage.thetimer = localStorage.thetimer-cost
+        this.button = document.createElement("button")
+        this.button.textContent = name
+        this.button.addEventListener("click",() =>{
+            if ((localStorage.thetimer-cost) > 0){
+                localStorage.thetimer = localStorage.thetimer-cost
+                func()
+            }
+
+        })
     }
 }
-regularbgchange =new item("background change cheap",100,false)
+regularbgchange = new item("background change cheap",10,changebg)
+shopbtn.addEventListener("click",()=>{
+    shopbtn.style.visibility = 'hidden'
+    shopclosebtn.style.visibility = 'visible'
+    regularbgchange.button.style.visibility = 'visible'
+    document.body.appendChild(regularbgchange.button)
+    expensivebgchange = new item("background change expensive",1000)
+})
+shopclosebtn.addEventListener("click",()=>{
+    shopbtn.style.visibility = 'visible'
+    shopclosebtn.style.visibility = 'hidden'
+    regularbgchange.button.style.visibility = 'hidden'
+    expensivebgchange = new item("background change expensive",1000)
+})
